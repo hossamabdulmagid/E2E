f12b065eff77e7f8ceb434bf386a90a6acfbe5a5
= <hosamabdulmaged@gmail.com>
2022-09-28 21:58:29 +0200

    remove text-decoration in link

--- e2e-frontend/src/pages/notes/notes.styles.jsx
+++ e2e-frontend/src/pages/notes/notes.styles.jsx
@@ -5,5 +5,12 @@ import {Link} from 'react-router-dom'
 export const LinK = styled(Link)`
   text-decoration: none;
   color: black;
-  
+
+  &:hover {
+    text-decoration: none;
+    color: black;
+
+  }
+
+
 `;
